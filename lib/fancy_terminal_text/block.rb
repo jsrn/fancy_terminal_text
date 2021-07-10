40c7e656cb1a21258e73d7126259684ec6cb2ff3
jsrn <jsrn@hey.com>
2021-07-10 23:48:49 +0100

    Lines, not components

--- lib/fancy_terminal_text/block.rb
+++ lib/fancy_terminal_text/block.rb
@@ -1,25 +1,29 @@
 module FTT
   class Block
-    attr_reader :components
+    attr_reader :lines
 
-    def initialize(*components)
-      @components = normalised_components(components)
+    # Create a block from one or more text objects. Each text object will
+    # become a line in the block.
+    #
+    # @param lines [Array<FTT::Text>]
+    def initialize(*lines)
+      @lines = normalised_lines(lines)
     end
 
     def to_s
-      @components.collect(&:to_s).join("\n")
+      @lines.collect(&:to_s).join("\n")
     end
 
     def visual_length
-      @components.collect(&:visual_length).sum
+      @lines.collect(&:visual_length).sum
     end
 
     def pad(top = 1, left = 1, right = 1, bottom = 1)
       top_padding = Array.new(top) { FTT::Text.new('') }
       bottom_padding = Array.new(bottom) { FTT::Text.new('') }
-      padded_top_and_bottom = self.class.new(*top_padding, *@components, *bottom_padding)
+      padded_top_and_bottom = self.class.new(*top_padding, *@lines, *bottom_padding)
 
-      padded_all = padded_top_and_bottom.components.map do |line|
+      padded_all = padded_top_and_bottom.lines.map do |line|
         l = line.left_pad(line.visual_length + left)
         l.right_pad(l.visual_length + right)
       end
@@ -30,17 +34,17 @@ module FTT
     def border(chars = FTT::Borders::HASH)
       top_border = ''
 
-      (components.first.visual_length + 2).times do
+      (lines.first.visual_length + 2).times do
         top_border << chars.sample
       end
 
-      middle = components.map do |c|
+      middle = lines.map do |c|
         FTT::Text.from_parts(FTT::Text.new(chars.sample), c, FTT::Text.new(chars.sample))
       end
 
       bottom_border = ''
 
-      (components.first.visual_length + 2).times do
+      (lines.first.visual_length + 2).times do
         bottom_border << chars.sample
       end
 
@@ -49,10 +53,14 @@ module FTT
 
     private
 
-    def normalised_components(components)
-      max_length = components.collect(&:visual_length).max
-      components.map do |component|
-        FTT::Text.new(component.to_s).right_pad(max_length)
+    # Right pad all lines in the block until all lines are equal to the visual
+    # length of the longest.
+    #
+    # @param lines [Array<FTT::Text>]
+    def normalised_lines(lines)
+      max_length = lines.collect(&:visual_length).max
+      lines.map do |line|
+        FTT::Text.new(line.to_s).right_pad(max_length)
       end
     end
   end
